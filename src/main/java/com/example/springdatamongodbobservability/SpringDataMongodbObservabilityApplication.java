07233b908449630f8d16117fed20aedd0438e63f
Greg L. Turnquist <gturnquist@vmware.com>
2022-10-25 11:01:50 -0500

    Catch up.

--- src/main/java/com/example/springdatamongodbobservability/SpringDataMongodbObservabilityApplication.java
+++ src/main/java/com/example/springdatamongodbobservability/SpringDataMongodbObservabilityApplication.java
@@ -1,25 +1,17 @@
 package com.example.springdatamongodbobservability;
 
+import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.tracing.Tracer;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.observation.ObservationRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.observability.EnableMongoObservability;
-import org.springframework.data.mongodb.observability.MongoMetricsConfigurationHelper;
+import org.springframework.data.mongodb.observability.ContextProviderFactory;
 import org.springframework.data.mongodb.observability.MongoObservationCommandListener;
-import org.springframework.data.mongodb.observability.MongoTracingObservationHandler;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.utility.DockerImageName;
-
-import com.mongodb.ConnectionString;
 
 @SpringBootApplication
-@EnableMongoObservability
 public class SpringDataMongodbObservabilityApplication {
 
 	public static void main(String[] args) {
@@ -28,42 +20,24 @@ public class SpringDataMongodbObservabilityApplication {
 
 	// Setting up MongoDB
 
-	@Bean(initMethod = "start", destroyMethod = "stop")
-	MongoDBContainer mongoDBContainer() {
-		return new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
-	}
-
 	@Bean
-	CommandLineRunner initDatabase(EmployeeRepository repository) {
+	CommandLineRunner initDatabase(EmployeeRepository repository, ObservationRegistry registry) {
 		return args -> {
-			repository.save(new Employee("Frodo", "ring bearer"));
-			repository.save(new Employee("Bilbo", "burglar"));
+			Observation.createNotStarted("init-database", registry).observe(() -> {
+				repository.deleteAll();
+				repository.save(new Employee("Frodo", "ring bearer"));
+				repository.save(new Employee("Bilbo", "burglar"));
+			});
 		};
 	}
 
 	// Configuring MongoDB + Micrometer
 
 	@Bean
-	MongoClientSettingsBuilderCustomizer mongoMetricsSynchronousContextProvider(Tracer tracer,
-			ObservationRegistry registry) {
+	MongoClientSettingsBuilderCustomizer mongoMetricsSynchronousContextProvider(ObservationRegistry registry) {
 		return (clientSettingsBuilder) -> {
-			clientSettingsBuilder.contextProvider( //
-					MongoMetricsConfigurationHelper.synchronousContextProvider(tracer, registry));
+			clientSettingsBuilder.contextProvider(ContextProviderFactory.create(registry))
+					.addCommandListener(new MongoObservationCommandListener(registry));
 		};
 	}
-
-	@Bean
-	MongoClientSettingsBuilderCustomizer mongoObservationCommandListenerCustomizer(MongoDBContainer mongoDBContainer,
-			MongoObservationCommandListener commandListener) {
-		return (clientSettingsBuilder) -> clientSettingsBuilder //
-				.applyConnectionString(new ConnectionString(mongoDBContainer.getConnectionString())) //
-				.addCommandListener(commandListener);
-	}
-
-	@Bean
-	ObservationRegistryCustomizer<ObservationRegistry> mongoTracingHandlerCustomizer(
-			MongoTracingObservationHandler handler) {
-		return handler::register;
-	}
-
 }
